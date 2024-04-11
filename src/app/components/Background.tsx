11c3a287d6e989ebdf8a2e329d8749285e514791
Théo Lévêque <theo.leveque@gmail.com>
2024-04-11 13:41:49 -0700

    feat: loader + bg on page

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -13,26 +13,24 @@ const Background = ({
   const colorStops = useGradientColorStops(isItDay, icon)
   const offsets = [2, 24, 46, 60, 81, 99]
   return (
-    <svg
-      width={window.innerWidth}
-      height={window.innerHeight}
-      className='fixed inset-0 -z-10 '
-    >
-      <defs>
-        <linearGradient id={id} x1='0%' y1='0%' x2='100%' y2='100%'>
-          {colorStops.map((color, i) => (
-            <stop key={i} offset={`${offsets[i]}%`} stopColor={color} />
-          ))}
-        </linearGradient>
-      </defs>
-      <rect
-        x='0'
-        y='0'
-        width='100%'
-        height='100%'
-        fill={`url(#chart-bg-gradient)`}
-      />
-    </svg>
+    <div className="fixed inset-0 -z-10">
+      <svg width="100%" height="100%">
+        <defs>
+          <linearGradient id={id} x1="50%" y1="0%" x2="75%" y2="100%">
+            {colorStops.map((color, i) => (
+              <stop key={i} offset={`${offsets[i]}%`} stopColor={color} />
+            ))}
+          </linearGradient>
+        </defs>
+        <rect
+          x="0"
+          y="0"
+          width="100%"
+          height="100%"
+          fill={`url(#chart-bg-gradient)`}
+        />
+      </svg>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import { useGlobalContext } from '@/lib/GlobalContext'
+import Background from './components/Background'
 import Graph from './components/Graph'
 import Header from './components/Header'
 
 export default function Home() {
+  const { isItDay, timestamp } = useGlobalContext()
   return (
     <main className="h-svh">
       <Header />
       <Graph />
+      <Background
+        icon={timestamp.icon}
+        id="chart-bg-gradient"
+        isItDay={isItDay}
+      />
     </main>
   )
 }
