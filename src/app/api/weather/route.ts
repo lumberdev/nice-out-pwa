0389565abed70bec1fdfc0e1926b6a34775a4f2a
Pratik <pratikmndh@gmail.com>
2024-05-02 18:08:03 -0400

    chore: update endpoints

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -3,7 +3,7 @@ export async function GET(req: Request) {
   const lat = searchParams.get('lat')
   const lon = searchParams.get('lon')
   try {
-    const url = `https://www.meteosource.com/api/v1/flexi/point?lat=${lat}&lon=${lon}&sections=all&timezone=auto&language=en&units=us&key=${process.env.METEO_AI_API_KEY}`
+    const url = `http://localhost:4000?lat=${lat}&lon=${lon}`
     const res = await fetch(url)
     const data = await res.json()
     return Response.json(data, { status: 200 })
